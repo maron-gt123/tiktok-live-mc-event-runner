0fd736332c68fc50c7a2336625e0c1afcf7f2346
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2025-12-14 21:18:18 +0900

    Update main.py

--- python/app/main.py
+++ python/app/main.py
@@ -6,17 +6,6 @@ TIKTOK_USER = os.environ.get("TIKTOK_USER")
 
 client = TikTokLiveClient(unique_id=TIKTOK_USER)
 
-@client.on(CommentEvent)
-async def on_comment(event: CommentEvent):
-    print(f"[COMMENT] {event.user.nickname}: {event.comment}")
-
-@client.on(GiftEvent)
-async def on_gift(event: GiftEvent):
-    if event.gift.streakable and event.streaking:
-        return
-    print(f"[GIFT] {event.user.nickname} -> {event.gift.name} x{event.repeat_count}")
-
-client.run()
 # すべてのイベントを捕まえる
 @client.on("event")
 async def on_any_event(event):
