b8bf06af2decd8efd9eb449e27e68da10d498feb
maron-gt123 <110322271+maron-gt123@users.noreply.github.com>
2026-01-03 13:19:46 +0900

    Update wrapper.py

--- python/app/wrapper.py
+++ python/app/wrapper.py
@@ -1,16 +1,23 @@
-import main
-import dummy_main
-
-import yaml
+import importlib.util
 from pathlib import Path
+import yaml
 
 CONFIG_PATH = Path("/app/config/config.yaml")
-
 with open(CONFIG_PATH, "r", encoding="utf-8") as f:
     config = yaml.safe_load(f)
 
 mode = config.get("mode", "production").lower()
 print(f"[WRAPPER] Mode is {mode}. Running corresponding main...")
+
+def load_module(name, path):
+    """指定パスからモジュールをロード"""
+    spec = importlib.util.spec_from_file_location(name, path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+main = load_module("main", "/app/main.py")
+dummy_main = load_module("dummy_main", "/app/dummy_main.py")
 
 if mode == "dummy":
     dummy_main.run_dummy()
